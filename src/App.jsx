f1f571062a9055a08e7620bb3b0a40f974fc6a3c
Erik Rodriguez <erikrodriguez@Eriks-Air.lan>
2024-02-23 13:49:04 -0600

    updated css & added yellow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 import './index.css'
 import { Routes, Route, Link } from 'react-router-dom'
 import Blue from './components/Blue'
 import Red from './components/Red'
 import Home from './components/Home'
+import Yellow from './components/Yellow'
 
 function App() {
 
@@ -14,16 +14,24 @@ function App() {
     <div id="container">
       <h1> </h1> 
       <div id="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/blue">This is blue</Link>
-        <Link to="/red">This is red</Link>
+        <Link className='home' to="/">Home</Link>
+        <Link className='blue' to="/blue">Blue</Link>
+        <Link className='red' to="/red">Red</Link>
+        <Link className='yellow' to="/yellow">Yellow </Link>
       </div>
       <div id="main-section">
         <Routes> 
           <Route path="/" element={<Home/>} />
           <Route path="/blue" element={<Blue/>} />
           <Route path="/red" element={<Red/>}/>
+          <Route path="/yellow" element={<Yellow/>}/>
         </Routes>
+        </div>
+        <div className='footer'> 
+        <Link className='home' to="/">Home</Link>
+        <Link className='blue' to="/blue">Blue</Link>
+        <Link className='red' to="/red">Red</Link>
+        <Link className='yellow' to="/yellow">Yellow </Link>
         </div>
     </div>
     </>
